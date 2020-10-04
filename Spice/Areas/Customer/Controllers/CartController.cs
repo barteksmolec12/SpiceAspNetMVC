43a8aa217937f9ac4e122ce438a1bd228c81063e
barteksmolec12 <barteksmolec@o2.pl>
2020-10-04 12:30:47 +0200

    Create CartController

---
+++ Spice/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Spice.Data;
+
+namespace Spice.Areas.Customer.Controllers
+{
+	public class CartController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CartController1(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
